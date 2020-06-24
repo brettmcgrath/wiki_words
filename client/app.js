8b1d022a73e74771c3f64fb242b06e79bdba6d6f
Brett McGrath <mcgrath@protonmail.com>
2020-06-23 23:09:00 -0400

    fourth commit

---
+++ client/app.js
@@ -0,0 +1,46 @@
+const superagent = require('./node_modules/superagent')
+
+const post_form = document.getElementById('post_form');
+
+
+post_form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const form_data = document.getElementById('wiki_url').value; //THIS RIGHT HERE NEEDS TO BE JSON
+    const form_json = { "url" : `${form_data}` } 
+    console.log(`this is form json ${form_json}`);
+    superagent
+      .post('http://127.0.0.1:8080/wiki_url')
+      .send(form_json)
+      .set('Content-Type', 'application/json')
+      .end((err, res) => {
+      })
+    });
+
+
+    var text = "This is a paragraph about Brett. Brett is doing some Javascript. Brett likes to write Javascript. Brett likes to eat fajitas. Brett eats a lot. Brett eats a lot while he codes in Javascript. ";
+    var lines = text.split(/[,\. ]+/g);
+    var data = Highcharts.reduce(lines, function (arr, word) {
+        var obj = Highcharts.find(arr, function (obj) {
+            return obj.name === word;
+        });
+        if (obj) {
+            obj.weight += 1;
+        } else {
+            arr.push({
+                name: word,
+                weight: 1
+            });
+        }
+        return arr;
+    }, []);
+    
+    Highcharts.chart('word-cloud', {
+        series: [{
+            type: 'wordcloud',
+            data: data,
+            name: 'Occurrences'
+        }],
+        title: {
+            text: 'Wordcloud of Lorem Ipsum'
+        }
+    });
--- server/index.js
+++ server/index.js
@@ -13,81 +13,87 @@ app.listen(`${port}`, () => console.log(`listening at http://localhost:${port}`)
 app.use(express.static('public'));
 
 
+let urls = []; //a list of all websites listed 
+let words = [];
+let words_str = "";
+let revision_list = [];
+let difference = "";
+let references = [];
+let final_text = "";
+let array47 = [];
+let text_array = [];
+
 app.post('/wiki_url', (request, response) => {
   const url = request.body['url'];
-  console.log(url);
+  //console.log(url);
 
   superagent
     .get(url)
     .end((error, res) => {
-      let page_number = Object.keys(res.body.query.pages)[0];
-      let extract = res.body.query.pages[page_number].extract;
-      console.log(res.body.query.pages["63948073"].revisions[10]['*']); //Write a for loop to iterate through the second to last item in the sequence to get the text blobs for 500 entires at a time
-      response.send();
-    })
+        let page_number = Object.keys(res.body.query.pages)[0]; //page number assigned to that particular wikipedia article
+        //let extract = res.body.query.pages[page_number].extract;
+        //let len = res.body.query.pages[page_number].revisions.length;
+        let revisions = res.body.query.pages[page_number].revisions; 
+        //[10]['*']);     this is the last part that it takes to get to the text of the revisions
+        //find_diff(res.body.query.pages["63948073"].revisions[i]['*']), (res.body.query.pages["63948073"].revisions[i+1]['*']);
+        response.send();
+
+
+        Object.keys(revisions).forEach(function (i) {
+            let revs = revisions[i]['*'];
+            revision_list.push(revs);
+        });
+        
+        let revision_str = revision_list.join(" ");
+            // console.log(clean(revision_str))
+
+
+        for (var i = 0; i < revision_list.length; i++)
+            Diff.diffWords(revision_list[i], revision_list[++i])
+            .forEach(function(part){
+            let removed = part.removed //boolean value
+            let added = part.added //boolean value
+            if (added || removed) {
+                difference += part.value + ' ';
+                
+            };
+        });
+        console.log(clean(difference))
+        //console.log(difference)
+     });
+    
 });
 
 
 
-//for testing purposes
-const one = "On 13 May 2020, Breonna Taylor, an [[emergency medical technician]], was allegedly shot during a [[narcotics]] [[investigation]] where [[Louisville Metro Police Department]] officers entered her [[apartment]] unannounced.  The investigation was supposedly centered around a '[[trap house]]' over 16 km away from the residence of the victim, as well as the two people that police believed were distributing [[controlled substances]].<ref>https://www.usatoday.com/story/news/nation/2020/05/13/breonna-taylor-not-target-louisville-police-investigation-when-shot/5181690002/</ref> The boyfriend of Taylor, Kenneth Walker, discharged his [[firearm]] first, injuring a law enforement officer.  Walker's lawyer stated that Walker thought that someone was entering the residence illegally, and that Walker acted only in self-defense.  Walker now faces criminal charges of first-degree assault and attempted murder of a police officer.<ref>https://www.courier-journal.com/story/news/crime/2020/05/13/breonna-taylor-lawyer-says-louisville-police-need-to-get-story-straight/5183137002/</ref>"
-const two = "On 13 May 2020, 26 y o Breonna Taylor, an [[emergency medical technician]], was allegedly shot eight times<ref>https://www.washingtonpost.com/nation/2020/05/11/family-seeks-answers-fatal-police-shooting-louisville-woman-her-apartment/</ref> during a [[narcotics]] [[investigation]] where [[Louisville Metro Police Department]] officers Jonathan Mattingly, Brett Hankison and Myles Cosgrove<ref>https://www.usatoday.com/story/news/nation/2020/05/12/breonna-taylor-killing-family-hires-attorney-ahmaud-arbery-case/3114440001/</ref> entered her [[apartment]] in [[Louisville]], [[Kentucky]] united states unannounced.  The investigation was supposedly centered around a '[[trap house]]' over 16 km away from the residence of the victim, as well as the two people that police believed were distributing [[controlled substances]].<ref>https://www.usatoday.com/story/news/nation/2020/05/13/breonna-taylor-not-target-louisville-police-investigation-when-shot/5181690002/</ref> The boyfriend of Taylor, Kenneth Walker, allegedly discharged his [[firearm]] first, injuring a law enforement officer.  Walker's lawyer stated that Walker thought that someone was entering the residence illegally, and that Walker acted only in self-defense.  Walker now faces criminal charges of first-degree assault and attempted murder of a police officer.<ref>https://www.courier-journal.com/story/news/crime/2020/05/13/breonna-taylor-lawyer-says-louisville-police-need-to-get-story-straight/5183137002/</ref> Attorney Ben Crump stated that “they already had the person they were searching for in custody.”<ref>https://www.wave3.com/2020/05/12/breonna-taylor-was-killed-botched-police-raid-attorney-says/</ref>"
-const three = "Some words to fill in a text blob. This is stuff I am writing."
-const four = "Hello, this is a test just to fill in some text that I need to have written for the fourth text blob."
-
-   // let new_stuff = cleaner_text.split(/^[\w][\.+]/); //filters URLS in text
-    
-let websites = [];
-let clean_array = [];
-let words = [];
-
-
  function clean(text) {
-    //this function takes a string of words and cleans out anything that isn't a space, number or letter
-    let text1 = text.replace(/<ref>/igm, ' ') //takes <ref> out of links and lets preceeding word free of URL
-    let text2 = text1.replace(/<\/ref>/igm,  ' ') //takes out end of URL freeing up the end
-    let text3 = text2.replace(/\/ref/igm,  ' ') //takes out end of URL freeing up the end
-    let text4 = text3.replace(/[.]ref/igm,  ' ') //takes out end of URL freeing up the end
-    let text5 = text4.replace(/[^\w\s\d\.\:\/]/gim, ''); //replaces [[ , > and other junk characters with space
-    let text_array = text5.split(/\s+/)    //splits a paragraph string into an array of individual words
-    // for (i=0; i < cleaner_text_array.length; i++) {
-        //if (cleaner_text_array[i].match(/^\w+[.]+\w+/gi)) { //filters URLS from array
-        for (element of text_array) {
-            if (element.match(/http/gim)) { //finds URLS
-                websites.push(element); //puts the uncleaned URLs into an array called websites
-            }
-        
-        }
-        let text_string = text_array.join(" ") //makes text string instead of array
-        let no_urls = text_string.replace(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/gim, ' ');
-        let no_periods = no_urls.replace(/\./gim, '');
-        let almost_array = no_periods.split(/\s+/)
-        for (element of almost_array) {
-            if (element.match(/http/gim)) {
-                continue;
-            } else {
-                words.push(element)
-            }
-        
+    text = text.replace(/\<r.*?>/igm, ' ')
+    .replace(/\<\/r.*?>/igm, ' ') //takes out any potential straggling /ref
+    .replace('web|url',  ' ')
+    .replace('|url',  ' ')
+    .replace('|language=en-US|work=', ' ')
+    .replace(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/gim, ' ')
+    .replace(/\[\[.*?\]\]/gim, ' ') //replace [[stuff]]
+    .replace(/{{.*?\}}/gim, ' ') //replace {{stuff}}
+    .replace(/\|/igm, ' ') //takes out | and replaces with a space
+    .replace('== References ==', ' ') //Replaces references tag
+    .replace(/[\.\:\;\,\"\'\>\<\-\+\=\”\“\/]/gim, ' ') //removes periods, :, commands and ; from string
+    .replace('date=', ' ')
+    .replace('name=', ' ')
+    .replace('reflist', ' ')
+    .replace('rfs', ' ')
+    .replace(/https?/, ' ')
+    .replace(/\n/g, ' ') //replace new lines with a single space
+    .replace(/  +/g, ' ') //replace multiple spacings with one
+    .replace(/\'[s]/gim, " ")
+    .replace(/\’[s]/gim, "")
+    
 
-        
-        }
-        let final_array = no_periods.split(/\s+/);
-        console.log(websites)
+    let replace_array = [" a ", " the ", " for ", " but ", " s ", " The ", " of ", " in ", " about ", " On ", " that ", " is ", " are "]
+    for (let i = 0; i < replace_array.length; ++i) {
+        if (text.includes(replace_array[i])) {
+            text = text.replace((replace_array[i]), ' ')
+            }
+    }
+    return text;
  };
-
-//clean(one);
-
-//JS Diff stuff below...this stuff gets the difference between two text blob
-function find_diff(text1, text2) {
-    diff_log = " "
-    const difference = Diff.diffWords(text1, text2);
-    const words = difference[0]["value"] //TO DO: iterate through the [0] to get value for each value object in arrays
-    for (i=0; i < difference.length; i++) {
-        diff_log += (" " + difference[i]["value"] + " ");
-        }
-
-    console.log(diff_log);
-}
-
-clean(two);
